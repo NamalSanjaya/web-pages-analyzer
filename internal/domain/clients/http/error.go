892c3178c319ab510e21a30f0a5de013ac78e788
NamalSanjaya <namalsanjaya23@gmail.com>
2025-07-04 21:38:31 +0530

    Add custom error for http related calls

---
+++ internal/domain/clients/http/error.go
@@ -0,0 +1,16 @@
+package http
+
+import "fmt"
+
+type httpError struct {
+	StatusCode int
+	Message    string
+}
+
+func NewHttpError(statusCode int, message string) *httpError {
+	return &httpError{StatusCode: statusCode, Message: message}
+}
+
+func (e *httpError) Error() string {
+	return fmt.Sprintf("HTTP call related error, status code %d: %s", e.StatusCode, e.Message)
+}
