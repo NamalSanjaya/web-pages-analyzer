bd4197d7ab12bd3d9ebe0fb7790a96741c8432ff
NamalSanjaya <namalsanjaya23@gmail.com>
2025-07-05 13:16:50 +0530

    Add Head http method

--- internal/domain/clients/http/interface.go
+++ internal/domain/clients/http/interface.go
@@ -4,6 +4,12 @@ import (
 	"net/http"
 )
 
+type HttpClientCfg struct {
+	Timeout      int // Timeout in seconds
+	MaxRedirects int
+}
+
 type HttpClient interface {
 	Get(url string) (*http.Response, error)
+	Head(url string) (*http.Response, error)
 }
--- internal/infrastructure/clients/http/http.go
+++ internal/infrastructure/clients/http/http.go
@@ -3,6 +3,7 @@ package http
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	clihttp "web-pages-analyzer/internal/domain/clients/http"
 )
@@ -11,9 +12,17 @@ type httpClient struct {
 	httpClient *http.Client
 }
 
-func New() clihttp.HttpClient {
+func New(cfg *clihttp.HttpClientCfg) clihttp.HttpClient {
 	return &httpClient{
-		httpClient: http.DefaultClient,
+		httpClient: &http.Client{
+			Timeout: time.Duration(cfg.Timeout) * time.Second,
+			CheckRedirect: func(req *http.Request, via []*http.Request) error {
+				if len(via) >= cfg.MaxRedirects {
+					return http.ErrUseLastResponse
+				}
+				return nil
+			},
+		},
 	}
 }
 
@@ -36,6 +45,29 @@ func (c *httpClient) Get(url string) (*http.Response, error) {
 	return resp, nil
 }
 
+func (c *httpClient) Head(url string) (*http.Response, error) {
+	resp, err := c.httpClient.Head(url)
+	if err != nil {
+		return nil, clihttp.NewHttpError(
+			http.StatusBadGateway,
+			fmt.Sprintf("error in HEAD call: %s", err.Error()),
+		)
+	}
+
+	if !isHeadSucceed(resp.StatusCode) {
+		return nil, clihttp.NewHttpError(
+			resp.StatusCode,
+			fmt.Sprintf("faliure in HEAD call: %s", resp.Status),
+		)
+	}
+
+	return resp, nil
+}
+
 func isSucceed(statusCode int) bool {
 	return statusCode >= 200 && statusCode < 300
+}
+
+func isHeadSucceed(statusCode int) bool {
+	return statusCode >= 200 && statusCode < 400
 }
